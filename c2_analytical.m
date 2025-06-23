bbecef00c3e9dfdcf895be94650d5e6d8dee1750
Mathieu Bignolles <mbig@mech.kth.se>
2025-06-23 15:42:46 +0200

    MAJ 23/06

--- c2_analytical.m
+++ c2_analytical.m
@@ -2,19 +2,18 @@
 % this is a quadratic function verified by c2 analytical 
 % Here we calculate the roots of it 
 
-function c2_ana_tilde = c2_analytical( r_a2, r_d2, c_total, s_inf, k_rat,integral_,integral_2)
+function c2_ana_tilde = c2_analytical( r_a2, r_d2, c_total, s_inf, k_rat,I1,I2)
 
 
-    a = r_a2 * (integral_ * (k_rat - 1) + 2);
-    b = (k_rat - 1) * integral_ * r_d2 + ...
-        6 * r_a2 * (integral_ - integral_2) * s_inf + ...
-        2*r_d2 - 2 * r_a2 * c_total;
+    a = r_a2 * (I1 * (k_rat - 1) + 2);
+    b = (I1*(k_rat-1)+2)*r_d2 + s_inf*I2*r_a2-2*r_a2*c_total;
     c = -2 * r_d2 * c_total;
 
     delta = b^2 - 4*a*c;
 
     if delta < 0
         c2_ana_tilde = NaN;  % No real root
+       
     elseif (delta == 0)
        disp('The equation has only one real roots');
        disp(-b./(2*a)); 
@@ -22,9 +21,9 @@ function c2_ana_tilde = c2_analytical( r_a2, r_d2, c_total, s_inf, k_rat,integra
         root1 = (-b + sqrt(delta)) / (2*a);
         root2 = (-b - sqrt(delta)) / (2*a);
 
-        % You can also use logic here to choose based on physical relevance
-        %c2_ana_tilde = min(root1, root2);  
-        c2_ana_tilde = (root1+root2)/2; 
+
+        c2_ana_tilde = max(root1, root2)/2;  
+        %c2_ana_tilde = abs(min(root1,root2)); 
 
     end
 end
